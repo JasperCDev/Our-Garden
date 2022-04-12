aae147a71133e3bc48ed573d73567dfdb2397f55
JasperCDev <jasperischauvin@gmail.com>
2022-04-12 18:33:32 -0500

    cleanup & refactor

--- pages/api/clicks.ts
+++ pages/api/clicks.ts
@@ -1,35 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { readFile, writeFile } from "../../util/fs";
 
+async function getClicks(res: NextApiResponse) {
+  const readRes = await readFile("database/clicks.txt");
+
+  if (!readRes.ok) {
+    res.status(500).json(readRes.error);
+    return;
+  }
+
+  res.status(200).json(readRes.data);
+  return;
+}
+
+async function updateClicks(req: NextApiRequest, res: NextApiResponse) {
+  const readRes = await readFile("database/clicks.txt");
+
+  if (!readRes.ok) {
+    res.status(500).json(readRes.error);
+    return;
+  }
+
+  const writeRes = await writeFile("database/clicks.txt", req.body.clicks);
+
+  if (!writeRes.ok) {
+    res.status(500).json(writeRes.error);
+    return;
+  }
+
+  res.status(200).json(writeRes.data);
+}
+
 const clicks = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case "GET":
-      const fileRes = await readFile("database/clicks.txt");
-
-      if (!fileRes.ok) {
-        res.status(500).json(fileRes.error);
-        return;
-      }
-
-      res.status(200).json(fileRes.data);
-      return;
+      return getClicks(res);
     case "PUT":
-      const readRes = await readFile("database/clicks.txt");
-
-      if (!readRes.ok) {
-        res.status(500).json(readRes.error);
-        return;
-      }
-
-      const writeRes = await writeFile("database/clicks.txt", req.body.clicks);
-
-      if (!writeRes.ok) {
-        res.status(500).json(writeRes.error);
-        return;
-      }
-
-      res.status(200).json(writeRes.data);
-      return;
+      return updateClicks(req, res);
     default:
       res.status(404).json("404");
       return;
