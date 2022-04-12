290c29922f64ebb81ee32c045f350f8722f753cc
JasperCDev <jasperischauvin@gmail.com>
2022-04-12 18:56:31 -0500

    connect to server with axios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
+import axios from "axios";
 import type { NextPage } from "next";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    axios
+      .put("/api/clicks", {
+        clicks: "123456789",
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }, []);
+
   return <div>Hello World</div>;
 };
 
