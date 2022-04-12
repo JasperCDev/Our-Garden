339987760384be84b122a8e61018e729289eff44
JasperCDev <jasperischauvin@gmail.com>
2022-04-12 17:29:13 -0500

    implement database and readFile

--- pages/api/clicks.ts
+++ pages/api/clicks.ts
@@ -1,7 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
 
-const clicks = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ clicks: 1 });
+const clicks = async (req: NextApiRequest, res: NextApiResponse) => {
+  fs.readFile("database/clicks.txt", (err, data) => {
+    if (err) {
+      const status = Number(err.code || 500);
+      res.status(status).json(err);
+      return;
+    }
+    res.status(200).json(data);
+  });
 };
 
 export default clicks;
