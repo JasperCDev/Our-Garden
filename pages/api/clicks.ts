cffbc0ddc7b92387170b2b2a3745d5b215f6c524
JasperCDev <jasperischauvin@gmail.com>
2022-04-12 18:10:51 -0500

    switch statement :

--- pages/api/clicks.ts
+++ pages/api/clicks.ts
@@ -2,12 +2,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { readFile } from "../../util/fs";
 
 const clicks = async (req: NextApiRequest, res: NextApiResponse) => {
-  const fileRes = await readFile("database/clicks.txt");
-  if (!fileRes.ok) {
-    res.status(500).json(fileRes.error);
-    return;
+  switch (req.method) {
+    case "GET":
+      const fileRes = await readFile("database/clicks.txt");
+      if (!fileRes.ok) {
+        res.status(500).json(fileRes.error);
+        return;
+      }
+      res.status(200).json(fileRes.data);
+      return;
+    default:
+      res.status(404).json("404");
   }
-  res.status(200).json(fileRes.data);
 };
 
 export default clicks;
