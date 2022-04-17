38e88fc69248921c1dec9d6448994971b317ba69
JasperCDev <jasperischauvin@gmail.com>
2022-04-17 08:37:46 -0500

    cleanup + new comment

--- util/useCountInterval.ts
+++ util/useCountInterval.ts
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { mutate } from "swr";
 import { ScopedMutator } from "swr/dist/types";
 import { updateClicks } from "../api/fetchers";
@@ -6,33 +6,12 @@ import { MILLISECONDS_SERVER_INTERVAL } from "./constants";
 import { GetClicks } from "./GetClicksSWR";
 import { ClickMap } from "./types";
 
-const defaultSessionClickMap = {
-  "0": { clicks: 0 },
-  "1": { clicks: 0 },
-  "2": { clicks: 0 },
-  "3": { clicks: 0 },
-  "4": { clicks: 0 },
-  "5": { clicks: 0 },
-  "6": { clicks: 0 },
-  "7": { clicks: 0 },
-  "8": { clicks: 0 },
-  "9": { clicks: 0 },
-  "10": { clicks: 0 },
-  "11": { clicks: 0 },
-  "12": { clicks: 0 },
-  "13": { clicks: 0 },
-  "14": { clicks: 0 },
-  "15": { clicks: 0 },
-};
-
 export default function useCountInterval<T>(mutateKey: string) {
   const { data: clickMap } = GetClicks();
 
-  const [sessionClickMap, setSessionClickMap] = useState<ClickMap>(
-    defaultSessionClickMap
-  );
+  const [sessionClickMap, setSessionClickMap] = useState<ClickMap>({});
 
-  /* state needs to be in refs so that I can access state within the setInterval callback */
+  /* state needs to be in refs so that I can access them within the setInterval callback */
   const clickMapRef = useRef({});
   clickMapRef.current = clickMap;
 
@@ -54,7 +33,7 @@ export default function useCountInterval<T>(mutateKey: string) {
       updateClicks(sessionClickMapRef.current)
         .then(() => mutateRef.current(mutateKeyRef.current))
         .then(() => {
-          // reset session after update
+          // generate new click map who's properties are equal to the current session click map minus the closure click map we defined before we called updateClicks. This is to prevent the clicks done during this update from being lost
           const newSessionClickMap: ClickMap = {};
           for (let key in Object.keys(session_click_map)) {
             newSessionClickMap[key] = {
@@ -63,6 +42,7 @@ export default function useCountInterval<T>(mutateKey: string) {
                 session_click_map[key].clicks,
             };
           }
+
           setSessionClickMap(newSessionClickMap);
         });
     }, MILLISECONDS_SERVER_INTERVAL);
