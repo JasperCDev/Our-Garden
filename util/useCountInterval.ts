36facc5418917e823bc5137bb640350f47d1006f
JasperCDev <jasperischauvin@gmail.com>
2022-04-17 10:22:12 -0500

    fixing what I broke

--- util/useCountInterval.ts
+++ util/useCountInterval.ts
@@ -6,10 +6,31 @@ import { MILLISECONDS_SERVER_INTERVAL } from "./constants";
 import { GetClicks } from "./GetClicksSWR";
 import { ClickMap } from "./types";
 
+const defaultSessionClickMap = {
+  "0": { clicks: 0 },
+  "1": { clicks: 0 },
+  "2": { clicks: 0 },
+  "3": { clicks: 0 },
+  "4": { clicks: 0 },
+  "5": { clicks: 0 },
+  "6": { clicks: 0 },
+  "7": { clicks: 0 },
+  "8": { clicks: 0 },
+  "9": { clicks: 0 },
+  "10": { clicks: 0 },
+  "11": { clicks: 0 },
+  "12": { clicks: 0 },
+  "13": { clicks: 0 },
+  "14": { clicks: 0 },
+  "15": { clicks: 0 },
+};
+
 export default function useCountInterval<T>(mutateKey: string) {
   const { data: clickMap } = GetClicks();
 
-  const [sessionClickMap, setSessionClickMap] = useState<ClickMap>({});
+  const [sessionClickMap, setSessionClickMap] = useState<ClickMap>(
+    defaultSessionClickMap
+  );
 
   /* state needs to be in refs so that I can access them within the setInterval callback */
   const clickMapRef = useRef({});
