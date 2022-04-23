6da55b9cdf62befe57fc2ef62762334ce85bb0a1
JasperCDev <jasperischauvin@gmail.com>
2022-04-23 00:46:42 -0500

    leaf fade-in

--- util/constants.ts
+++ util/constants.ts
@@ -2,6 +2,6 @@ export const DATABASE_PATH = "database/clicks.txt";
 
 export const GET_CLICKS_SWR_KEY = "/api/clicks";
 
-export const MILLISECONDS_SERVER_INTERVAL = 1000;
+export const MILLISECONDS_SERVER_INTERVAL = 3000;
 
 export const ESTIMATED_SERVER_RESPONSE_TIME_MS = 200;
