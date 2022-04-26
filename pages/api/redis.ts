d50ca487c541c2b2d3c6fe6062910e7f8484e7a9
JasperCDev <jasperischauvin@gmail.com>
2022-04-25 21:58:31 -0500

    add redis logic

--- pages/api/redis.ts
+++ pages/api/redis.ts
@@ -2,9 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import redisClient from "util/redis";
 
 const redis = async (req: NextApiRequest, res: NextApiResponse) => {
-  await redisClient.set("ping", "pong");
-  const test = await redisClient.get("ping");
-  res.status(200).json(test);
+  let clickMap = await redisClient.hmget("clickMap", "0", "1", "2");
+  if (!clickMap) {
+    await redisClient.hmset("clickMap", ["0", "0", "1", "0", "2", "0"]);
+    clickMap = await redisClient.hmget("clickMap", "0", "1", "2");
+  }
+  if (req.method === "PUT") {
+    redisClient.hmset("clickMap", req.body.clickMap);
+    res.status(200).json(null);
+  }
+  res.status(200).json(clickMap);
 };
 
 export default redis;
