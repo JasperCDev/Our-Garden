25144019216ab6eec2d1b68660ec4e85c71ab338
JasperCDev <jasperischauvin@gmail.com>
2022-04-26 16:34:13 -0500

    get keys and values - redis

--- pages/api/redis.ts
+++ pages/api/redis.ts
@@ -2,16 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import redisClient from "util/redis";
 
 const redis = async (req: NextApiRequest, res: NextApiResponse) => {
-  let clickMap = await redisClient.hmget("clickMap", "0", "1", "2");
+  let clickMap = await redisClient.hgetall("clickMap");
   if (!clickMap) {
     await redisClient.hmset("clickMap", ["0", "0", "1", "0", "2", "0"]);
-    clickMap = await redisClient.hmget("clickMap", "0", "1", "2");
+    clickMap = await redisClient.hgetall("clickMap");
   }
   if (req.method === "PUT") {
     redisClient.hmset("clickMap", req.body.clickMap);
     res.status(200).json(null);
   }
-  res.status(200).json(clickMap);
+  res.status(200).send(clickMap);
 };
 
 export default redis;
