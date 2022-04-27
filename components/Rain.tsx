4a05a41cf7e84143f159967573cf5ce20867c661
JasperCDev <jasperischauvin@gmail.com>
2022-04-26 20:02:07 -0500

    static rain svg

--- components/Rain.tsx
+++ components/Rain.tsx
@@ -4,5 +4,20 @@ import styles from "./Rain.module.scss";
 interface Props {}
 
 export default function Rain(props: Props) {
-  return <svg className={styles.rain}></svg>;
+  return (
+    <svg className={styles.rain} width="100%" height="100%">
+      {new Array(100).fill(1).map((_, indx) => {
+        const randomWidth = Math.random() * window.innerWidth;
+        const randomHeight = Math.random() * window.innerHeight;
+        return (
+          <path
+            key={indx}
+            d={`M${randomWidth} 0 L${randomWidth} ${randomHeight}`}
+            stroke="lightblue"
+            strokeWidth="1"
+          ></path>
+        );
+      })}
+    </svg>
+  );
 }
