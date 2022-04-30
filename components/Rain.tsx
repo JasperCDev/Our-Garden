c20e376c3b2b4525f080ae8fcbf693288ae5a8b6
JasperCDev <jasperischauvin@gmail.com>
2022-04-29 19:54:49 -0500

    different raindrop count for different screen sizes

--- components/Rain.tsx
+++ components/Rain.tsx
@@ -5,9 +5,11 @@ import RainDrop from "./RainDrop";
 interface Props {}
 
 export default function Rain(props: Props) {
+  let rainCount = Math.floor(window.innerWidth / 10);
+
   return (
     <svg className={styles.rain} width="100%" height="100%">
-      {new Array(200).fill(1).map((_, indx) => {
+      {new Array(rainCount).fill(1).map((_, indx) => {
         const randomWidth = Math.random() * window.innerWidth;
         const randomHeight =
           Math.random() * window.innerHeight + window.innerHeight * 0.2;
