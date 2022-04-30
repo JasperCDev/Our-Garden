f9009f250bf2eb6e073f454a02b329ffa143c814
JasperCDev <jasperischauvin@gmail.com>
2022-04-29 21:33:44 -0500

    refactor

--- components/RainDrop.tsx
+++ components/RainDrop.tsx
@@ -7,27 +7,33 @@ interface Props {
   randomWidth: number;
 }
 
+const pathLength = 1;
+
 export default function RainDrop(props: Props) {
-  const animationDelay = useMemo(() => Math.random() * 3, []);
+  const animationDelay = useMemo(() => `${Math.random() * 3}s`, []);
+  const animationDuration = `${props.randomHeight / window.innerHeight}s`;
+  const translateY = `${-(props.randomHeight * 2)}px`;
+
+  const strokeDasharray = `${50 / props.randomHeight} ${
+    (props.randomHeight - 50) / props.randomHeight
+  }`;
+
+  const d = `M${props.randomWidth} 0 L${props.randomWidth} ${props.randomHeight}`;
+
+  const customProperties = {
+    "--path-length": 1,
+    "--animation-duration": animationDuration,
+    "--translate-y": translateY,
+    "--animation-delay": `${animationDelay}s`,
+  } as CSSProperties;
 
   return (
     <path
       className={styles.rainDrop}
-      d={`M${props.randomWidth} 0 L${props.randomWidth} ${props.randomHeight}`}
-      strokeDasharray={`${50 / props.randomHeight} ${
-        (props.randomHeight - 50) / props.randomHeight
-      }`}
-      pathLength={1}
-      style={
-        {
-          "--path-length": 1,
-          "--animation-duration": `${(
-            props.randomHeight / window.innerHeight
-          ).toFixed(2)}s`,
-          "--translate-y": `${-(props.randomHeight * 2)}px`,
-          "--animation-delay": `${animationDelay}s`,
-        } as CSSProperties
-      }
+      d={d}
+      strokeDasharray={strokeDasharray}
+      pathLength={pathLength}
+      style={customProperties}
     ></path>
   );
 }
