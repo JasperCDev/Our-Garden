bd1a58ab24e4e2f060b531c0c9170ad57b84ac6c
JasperCDev <jasperischauvin@gmail.com>
2022-04-30 14:13:41 -0500

    raindrops shorter and slower

--- components/RainDrop.tsx
+++ components/RainDrop.tsx
@@ -11,17 +11,19 @@ const pathLength = 1;
 
 export default function Raindrop(props: Props) {
   const animationDelay = useMemo(() => `${Math.random() * 3}s`, []);
-  const animationDuration = `${props.randomHeight / window.innerHeight}s`;
+  const animationDuration = `${
+    props.randomHeight / (window.innerHeight * 0.5)
+  }s`;
   const translateY = `${-(props.randomHeight * 2)}px`;
 
-  const strokeDasharray = `${50 / props.randomHeight} ${
-    (props.randomHeight - 50) / props.randomHeight
+  const strokeDasharray = `${25 / props.randomHeight} ${
+    (props.randomHeight - 25) / props.randomHeight
   }`;
 
   const d = `M${props.randomWidth} 0 L${props.randomWidth} ${props.randomHeight}`;
 
   const customProperties = {
-    "--path-length": 1,
+    "--path-length": pathLength,
     "--animation-duration": animationDuration,
     "--translate-y": translateY,
     "--animation-delay": animationDelay,
