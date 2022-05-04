cddf425e331aaf367ed3a183d7ee31d58840d68c
JasperCDev <jasperischauvin@gmail.com>
2022-05-03 21:19:53 -0500

    prevent pop-in on load

--- util/GetClicksSWR.ts
+++ util/GetClicksSWR.ts
@@ -1,8 +1,9 @@
 import useSWR from "swr";
 import { getClicks } from "../api/fetchers";
 import { GET_CLICKS_SWR_KEY } from "./constants";
+import defaultClickMap from "./defaultClickMap";
 
 export function GetClicks() {
   const swrResponse = useSWR(GET_CLICKS_SWR_KEY, getClicks);
-  return { ...swrResponse, data: swrResponse.data || {} };
+  return { ...swrResponse, data: swrResponse.data || defaultClickMap };
 }
